42cfc9031a3578493b88341d7fd13dfa19e9c168
mannumishra <mannumishra2207@outlook.com>
2024-07-30 22:01:17 +0530

    add chnages

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async() => {
     try {
-        await mongoose.connect("mongodb+srv://hiteshy468:MWaYu4DttSO4gncU@cluster0.h0iedd9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect("mongodb+srv://mannu22072000:2OxqlDWMMsweOj9u@cluster0.rvg2lap.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log('MongoDB is Connected')
     } catch (error) {
         console.log(error)
